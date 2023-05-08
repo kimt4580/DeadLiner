0037a99df99284d7ee7a7bc5c55e752fb99affb4
kimt4580 <xogns8696@gmail.com>
2023-05-08 15:35:06 +0900

    ✨Feat : ListCellViewModel에 init 추가

--- DeadLiner/ViewModel/ListCellViewModel.swift
+++ DeadLiner/ViewModel/ListCellViewModel.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
-final class ListCellViewModel: ObservableObject {
+final class ListCellViewModel: TaskService {
   @Published var isListRowTapped = false
+  var task: Task
+  
+  init(isListRowTapped: Bool = false, task: Task, withService: TaskManagerService) {
+    self.isListRowTapped = isListRowTapped
+    self.task = task
+    super.init(withService: withService)
+  }
   
   func listRowTapped() {
     isListRowTapped.toggle()
   }
-  
 }
